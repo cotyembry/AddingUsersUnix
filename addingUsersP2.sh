837714a438d6fa5863c1d6e6ba57c861fdf0b2a7
Coty Embry <cotyembry@gmail.com>
2016-11-13 19:41:42 -0600

    ready to be submitted - but I have questions...

--- addingUsersP2.sh
+++ addingUsersP2.sh
@@ -2,30 +2,43 @@
 
 #/usr/local/bin/bash #not using because of developing on my Mac
 
-#Author:	John Coty Embry
-#Date:		11-10-2016
+#         Author:		John Coty Embry
+#           Date:		11-10-2016
+#Program Comment:		This script will erase and overwrite a new_users.txt file and output its own text to the new_users.txt file (or will create the new_users.txt file if it doens't exist) within the same level of the directory tree relative to where this script is ran
 
-#-------
+
+#-------Assignment
 #Write a script that will take input from cs_roster.txt made available by the IT department and generate the new_users.txt file that is used as input for the script in Adding Users - Part 1.
 
-#Please note:
-#	1. No accounts should be created for students enrolled in CMPSC 1513
-#	2. No accounts should be created for students enrolled in any course with CPSMA as a prefix except CPSMA 2922 which is our Data Structures class.
-#	3. The major code for Computer Science is 0510
-#	4. Any student that does not have Computer Science declared as his/her first or second major should have their home directory in /home/STUDENTS/majors
-#	5. Any student that does not have Computer Science as a major should have home directory in /home/STUDENTS/nonmajors
-
+# No accounts should be created for students enrolled in CMPSC 1513
+# No accounts should be created for students enrolled in any course with CPSMA as a prefix except CPSMA 2923 which is our Data Structures class
+# The major code for Computer Science is 0510.
+# Any student that has declared Computer Science as his/her first or second major should have their home directory in /home/STUDENTS/majors
+# Any student that does not have Computer Science as a major should have home directory in /home/STUDENTS/nonmajors
 #use the cs_roster.txt file for input directory from my home directory /home/faculty/mkt
 #-------
 
+#helpers for later
 majorCodeForComputerScience=0510
 majorDirectory='/home/STUDENTS/majors'
 nonmajorDirectory='/home/STUDENTS/nonmajors'
+shellDirectory='/user/local/bin/bash'
+
+#before starting, I will clear out the file then later I will just append to the file
+echo -n '' > new_users.txt #-n means do not include a newline at the end
 
 (
 while read line; do
 		
 	username=$(echo $line | cut -d '|' -f2)
+#	Here I will make an assumption and say if username is empty then the line is blank and I will skip blank lines for the output
+#	echo ">${username}<"
+	emptyLineFlag=0
+	if [ "$username" == "" ]; then
+		emptyLineFlag=1
+	fi
+
+
 	password=x
 	uid=x
 	gid=x
@@ -39,8 +52,6 @@ while read line; do
 	doNotCreateUser=0 #default it to false before starting the logic
 
 	isMajor=0 #I will use this to help me determine whether to put the student in /home/STUDENTS/majors or not
-
-
 
 	if [ "$coursePrefix" == "CMPSC" ]; then
 		if [ "$courseNumber" == "1513"  ]; then
@@ -65,21 +76,23 @@ while read line; do
 	#continue to step 3.
 	#to add the user or not...check using above flag. Then its time to go either major or nonmajor
 
-	if [ "$doNotCreateUser" == 0  ]; then
-		#see if the major code of this person is a Computer Science major
-		if [ "$majorCode" == "$majorCodeForComputerScience"  ]; then
-			echo $fullname $majorDirectory
-		else
-			echo $fullname	$nonmajorDirectory
+	#also make sure to skip empty lines in the file
+	if [ "$emptyLineFlag" == "0" ]; then
+		if [ "$doNotCreateUser" == 0  ]; then
+			#see if the major code of this person is a Computer Science major
+			if [ "$majorCode" == "$majorCodeForComputerScience"  ]; then
+				#the format for the line to output is
+				#username:password:uid:gid:GECOS:homedir:shell
+				# echo $fullname $majorDirectory
+				echo ${username}:${password}:${uid}:${gid}:${GECOS}:${majorDirectory}:${shellDirectory} >> new_users.txt
+			else
+				#the format for the line to output is
+				#username:password:uid:gid:GECOS:homedir:shell
+				# echo $fullname	$nonmajorDirectory
+				echo ${username}:${password}:${uid}:${gid}:${GECOS}:${nonmajorDirectory}:${shellDirectory} >> new_users.txt
+			fi
+
 		fi
-
-	fi
-
-
-
-
-
+	fi		
 done
 ) < cs_roster.txt
-
-
