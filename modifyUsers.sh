3b8c4a04f2067d186e68bfed761db8ad86ec1a42
Coty Embry <cotyembry@gmail.com>
2016-11-22 08:57:35 -0600

    added etc folder

--- modifyUsers.sh
+++ modifyUsers.sh
@@ -6,21 +6,41 @@
 #Author:	John Coty Embry
 #  Date:	11-10-2016
 
+#-------
+#Name Changes 
+#Note: May include both username and GECOS field modifications
+#Major Changes
+#Note: May include changing to or from Computer Science major and should result in appropriate placement of home directory.
+#-------
 
 echo -n '' > changeMe.txt #to clear the file out
 
 (
 while read line; do
+	#bijach:Acharya, Bijay:222446
+	
+	ecuid=$(echo $line | cut -d ':' -f3)
+	fullname=$(echo $line | cut -d ':' -f2)
+
+
+	#echo "usermod -l newUsername oldUsername"
+	#usermod –c “User for transfer files” transfer_user
+
+	#has the name changed? Time to find out
+	(
+		while read line2; do
+			echo $line2
+
+		done
+	) < /etc/passwd
 
 
 
-	username=$(echo $line | cut -d ':' -f1)
-	GECOS=$(echo $line | cut -d ':' -f5)
-	homedir=$(echo $line | cut -d ':' -f6)
 
-	echo "useradd -md $homedir -c \"${GECOS}\" -s /user/local/bin/bash -k /home/csadmin/SKEL $username" >> changeMe.txt
+	#echo "usermod -c \"${}${}\""
+
 
 
 done
-) < new_users.txt
+) < active_cs.txt
 
